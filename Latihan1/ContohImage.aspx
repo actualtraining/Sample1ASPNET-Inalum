75d0c27cf18c47784634046c626de8c8fc3d8550
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-05-15 15:35:57 +0700

    menambahkan image control

---
+++ Latihan1/ContohImage.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ContohImage.aspx.cs" Inherits="Latihan1.ContohImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Menampilkan Image Control</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="imgPegawai" Width="150" runat="server" /><br /><br />
+            <asp:Button Text="Show Random Images" ID="btnRandom" runat="server" OnClick="btnRandom_Click" /><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Latihan1/ContohImage.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Latihan1
+{
+    public partial class ContohImage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRandom_Click(object sender, EventArgs e)
+        {
+            Random objRnd = new Random();
+            switch (objRnd.Next(3))
+            {
+                case 0:
+                    imgPegawai.ImageUrl = "~/Images/1.jpg";
+                    imgPegawai.AlternateText = "pic kosong";
+                    break;
+                case 1:
+                    imgPegawai.ImageUrl = "~/Images/2.jpg";
+                    imgPegawai.AlternateText = "pic kosong";
+                    break;
+                case 2:
+                    imgPegawai.ImageUrl = "~/Images/3.jpg";
+                    imgPegawai.AlternateText = "pic kosong";
+                    break;
+            }
+        }
+    }
+}
